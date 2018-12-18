2dbd49ef961cca1527bf873e88b46edb04545c91
cola <Wlafica1!>
2018-12-18 18:15:47 +0100

    uhuh it can get my message from the spoken question

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill
+from mycroft.skills.core import MycroftSkill, intent_handler
+
 from mycroft.util.log import getLogger
+LOGGER = getLogger(__name__)
 
 import os
+
 import pynput
 keyboard = pynput.keyboard.Controller()
 
 __author__ = 'cola'
-
-LOGGER = getLogger(__name__)
 
 
 class VoiceControlSkill(MycroftSkill):
@@ -28,11 +29,6 @@ class VoiceControlSkill(MycroftSkill):
         self.register_intent(table_intent,
                              self.handle_table_intent)
 
-        typewriter_intent = IntentBuilder("TypeWriterIntent"). \
-            require("TypeWriterKeyword").build()
-        self.register_intent(typewriter_intent,
-                             self.handle_typewriter_intent)
-
     def handle_beamer_intent(self, message):
         beamer_screen()
         self.speak_dialog("changes.done")
@@ -41,21 +37,30 @@ class VoiceControlSkill(MycroftSkill):
         table_screen()
         self.speak_dialog("changes.done")
 
+    @intent_handler(IntentBuilder("TypeWriterIntent").require("TypeWriterKeyword").require("Text"))
     def handle_typewriter_intent(self, message):
-        type(message)
+        text = message.data.get('Text')
+        print( text )
+        # type(message)
         self.speak_dialog("changes.done")
 
     def stop(self):
         pass
+
 
 def create_skill():
     return VoiceControlSkill()
 
 
 def type( message ):
-    print( " start typing " )
-    print( dir(message) )
-    #keyboard.type(message)
+    # print( " start typing " )
+    # # print((LOGGER))
+    # print( " before list show " )
+    # for s in dir(message):
+    #     print( s )
+    text = message.data.get('Text')
+    print( text )
+
 
 def beamer_screen():
     os.system("xrandr --output DVI-I-2 --mode 1920x1080 --output  DVI-I-3 --off --output HDMI-0 --mode 1920x1080")
